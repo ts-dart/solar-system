2e34c69993670d2083e157b03d2ff6ee0bf09241
thiago <thiagoedusan5.11@outlook.com>
2023-02-09 22:12:00 -0300

    Alterando imagem de fundo dinamicamente, a imagem vem de uma api da nasa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import Missions from './components/Missions';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { urlImage: '' };
+  }
+
+  async componentDidMount() {
+    console.log('rodou mount');
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DfGAgSgjHyOLIH1UQFY36tUt14IWtsVr1N4pxJz7');
+    const data = await response.json();
+    console.log(data.url);
+
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({ urlImage: data.url });
+  }
+
+  componentDidUpdate() {
+    console.log('rodou update');
+    const { urlImage } = this.state;
+    document.body.style.backgroundImage = `url(${urlImage})`;
+  }
+
   render() {
+    console.log('rodou render');
     return (
       <div>
         <Header />
